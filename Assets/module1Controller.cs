f246870103f47ab036496456608bd023a9ca040e
Ragougnouk <arthur.brena@hotmail.fr>
2023-11-04 12:16:39 +0100

    module1

---
+++ Assets/module1Controller.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class module1Controller : MonoBehaviour
+{
+    public GameObject cadre;
+    public GameObject pointeur;
+    public GameObject sonar;
+    private Vector2 coordC;
+    private AudioSource sound;
+
+    public float size = 1.0f;
+
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coordC = cadre.transform.position;
+        sound = sonar.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float dirX = pointeur.transform.position.x + Input.GetAxis("Horizontal") * speed;
+        float dirY = pointeur.transform.position.y + Input.GetAxis("Vertical") * speed;
+        dirX = Mathf.Clamp(dirX,coordC.x-size,coordC.x + size);
+        dirY = Mathf.Clamp(dirY,coordC.y-size,coordC.y + size);
+
+        pointeur.transform.position = new Vector3(dirX,dirY,0);
+
+        soundUpdate();
+    }
+
+    void soundUpdate()
+    {
+        if (Vector3.Distance(sonar.transform.position,pointeur.transform.position) < sound.maxDistance)
+        {
+            sound.volume = 1 - (Vector3.Distance(sonar.transform.position,pointeur.transform.position) / sound.maxDistance);
+        }
+    }
+}
